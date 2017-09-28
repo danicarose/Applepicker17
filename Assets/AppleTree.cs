d9d24e8fa7a6afdb5f547d0550778d24fc37657e
danicarose <danica.calusin@gmail.com>
2017-09-28 13:04:11 -0400

    incomplete_9_28
    - created all game objects
    - completed AppleTree C# script

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+	//Prefab for instantiating apples 
+	public GameObject applePrefab;
+
+	//Speed at which the AppleTree moves in meteres/sec
+	public float speed = 1f;
+
+	//Distance to where AppleTree turns around 
+	public float leftAndRightEdge = 10f;
+
+	//Chance that AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+
+	//Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+
+
+	void Start () {
+		//Dropping apples every second
+	}
+
+	void Update () {
+		//Basic Movement
+		//Changing Direction
+	}
+}
