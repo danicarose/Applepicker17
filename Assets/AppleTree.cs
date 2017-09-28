d41d7eb5543f1f1dc3361071c6eb23ebfc7d23a1
danicarose <danica.calusin@gmail.com>
2017-09-28 13:44:59 -0400

    unfinished 2_28 (1)
    - need to start setting GameObject layers

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -21,10 +21,28 @@ public class AppleTree : MonoBehaviour {
 
 	void Start () {
 		//Dropping apples every second
+		InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+
+	void DropApple() {
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
 	}
 
 	void Update () {
 		//Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime; 
+		transform.position = pos;
+
 		//Changing Direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //Move right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); //Move left
+		} else if (Random.value < chanceToChangeDirections) {
+			speed *= -1; //Change direction
+		}
+
 	}
 }
