c5dd8d53cff3ca427a3fc9085f2e1901990b45f8
danicarose <danica.calusin@gmail.com>
2017-09-29 16:12:54 -0400

    unfinished 9_29
    - baskets won't move
    - apple instances don't disappear when collide w basket

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public static float bottomY = -20f;
+
 	
 	// Update is called once per frame
 	void Update () {
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+		}
 		
 	}
 }
