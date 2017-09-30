2166f0e7e56a33c14e9ac90e8c537c1e05aee428
DRC128 <DRC128@CILABS-119B-15.scilsnet.rutgers.edu>
2017-09-29 20:59:10 -0400

    unfinished hella errors 9_29 (1)
    - finished all code
    - need to go through and fix bugs regarding deleting all apples, gettind rid of baskets etc.

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour {
 
+    public GUIText scoreGT;
+
 	void Update () {
 		 // Get the current screen position of the mouse from Input
 		        Vector3 mousePos2D = Input.mousePosition;                           // 1
@@ -19,13 +21,39 @@ public class Basket : MonoBehaviour {
 		        this.transform.position = pos;
 	}
 
-	
-	void OnCollisionEnter(Collision coll){
+    private void Start()
+    {
+        //Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        //Get the GUIText Component of that GameObject
+        scoreGT = scoreGO.GetComponent<GUIText>();
+
+        //Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
+    void OnCollisionEnter(Collision coll){
 		//Find out what hit this basket 
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") {
 			Destroy (collidedWith);
 		}
+
+        //Parse the text of the scoreGT into an int 
+        int score = int.Parse(scoreGT.text);
+
+        //Add points for catching the apple 
+        score += 100;
+
+        //Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
+
+        //Track the high score
+        if(score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
 	}
 
 }
